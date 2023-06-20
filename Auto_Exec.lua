5dd327f2f18434dd630cda560e5caf59fa6461ae
KangKung <87695599+KangKung02@users.noreply.github.com>
2023-06-20 21:27:35 +0700

    Update Auto_Exec.lua

--- Auto_Exec.lua
+++ Auto_Exec.lua
@@ -1,1 +1,12 @@
-UwU
+local Key = _G.Key
+local DiscordId = _G.DiscordId
+
+queue_on_teleport(string.format([[
+    _G.Key = "%s"
+    _G.DiscordId = "%s"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))()
+]], Key, DiscordId))
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/just-bin/main/Auto_Exec.lua"))()
+print("Executed!")
