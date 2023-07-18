9cebad456356d3d3a2e20e98405f26d5b6c1e589
KangKung <87695599+KangKung02@users.noreply.github.com>
2023-07-18 23:09:19 +0700

    Update Auto_Exec.lua

--- Auto_Exec.lua
+++ Auto_Exec.lua
@@ -2,12 +2,15 @@ local Key = _G.Key
 local DiscordId = _G.DiscordId
 
 queue_on_teleport(string.format([[
+    repeat wait() until game:IsLoaded() and not game.Players.LocalPlayer.PlayerGui:FindFirstChild("Loading")
+
     _G.Key = "%s"
     _G.DiscordId = "%s"
+
+
+    local function a(b)local c;while wait(1)do c=game:HttpGet(b)if#c>0 then break end;print("Retry http get!!!")end;return c end
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/just-bin/main/Auto_Exec.lua"))()
 ]], Key, DiscordId))
 
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))()
-
-print("Executed!");
+print("Executed!")
