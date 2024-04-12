26b6cc5f653de692f0a4403835c8c678588637d0
KangKung <87695599+KangKung02@users.noreply.github.com>
2024-04-12 13:49:47 +0700

    Update loop_auto_exec.lua

--- loop_auto_exec.lua
+++ loop_auto_exec.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet(Url .. "/test/Execute.lua"))()
-queue_on_teleport("getgenv().Url = " .. Url .. ";" .. [[loadstring(game:HttpGet(""))()]])
+queue_on_teleport("getgenv().Url = " .. Url .. ";" .. [[loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/just-bin/main/loop_auto_exec.lua"))()]])
